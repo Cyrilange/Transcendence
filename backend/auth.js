e1f657d24d1c1c928e8ce4303a390acba04f9243
Cyrilange <cyril.r.a.salamite@gmail.com>
2026-04-01 14:39:09 +0200

    installed Oauth2 with 42

--- backend/auth.js
+++ backend/auth.js
@@ -1,17 +1,17 @@
 require('dotenv').config()
 const axios = require('axios')
 
-// recup  token
-async function getToken() { // this function is to get the OAuth2 from api42
+async function getToken() {
   try {
-    const response = await axios.post('https://api.intra.42.fr/oauth/token', {
-      grant_type: 'client_credentials',
-      client_id: process.env.CLIENT_ID,
-      client_secret: process.env.CLIENT_SECRET
-    })
-    return response.data.access_token
+      const response = await axios.post('https://api.intra.42.fr/oauth/token', {
+          grant_type: 'client_credentials',
+          client_id: process.env.CLIENT_ID,
+          client_secret: process.env.CLIENT_SECRET
+      })
+      return response.data.access_token
   } catch (err) {
-    console.error(err.response.data)
+      console.error(err.response?.data || err.message)
+      throw err  // ← propage l'erreur au lieu de retourner undefined
   }
 }
 
--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,61 @@
+require('dotenv').config()
 const express = require('express')
+const axios = require('axios')
+const db = require('./database')
 const { getUser } = require('./callApi')
+
 const app = express()
 const port = 3000
 app.use(express.json())
-const db = require('./database')
 
-app.get('/user/:login', async (req, res) => { // exemple /user/csalamit
-  try { 
-    const data = await getUser(req.params.login) //endpoint  .login
-    res.json(data.login) // format json as node.js work like that
+app.get('/auth/login', (req, res) => {
+    const params = new URLSearchParams({
+        client_id: process.env.CLIENT_ID,
+        redirect_uri: process.env.REDIRECT_URI,
+        response_type: 'code'
+    })
+    res.redirect(`https://api.intra.42.fr/oauth/authorize?${params}`)
+})
+
+app.get('/', async (req, res) => {
+  const code = req.query.code
+  if (!code)
+      return res.send('hello world')
+  try {
+      const response = await axios.post('https://api.intra.42.fr/oauth/token', {
+          grant_type: 'authorization_code',
+          client_id: process.env.CLIENT_ID,
+          client_secret: process.env.CLIENT_SECRET,
+          code: code,
+          redirect_uri: process.env.REDIRECT_URI
+      })
+      const token = response.data.access_token
+      const userRes = await axios.get('https://api.intra.42.fr/v2/me', {
+          headers: { Authorization: `Bearer ${token}` }
+      })
+      const user = userRes.data
+      res.json({ login: user.login, email: user.email, avatar: user.image.link })
   } catch (err) {
-    res.status(err.response?.status || 500).json(err.response?.data || err.message)
+      res.status(500).json({ error: err.message })
   }
 })
 
-app.get('/db-test', (req, res) => {
-  db.all('SELECT * FROM users', [], (err, rows) => {
-      if (err)
-          return res.status(500).json({ error: err.message })
-      res.json(rows)
-  })
+app.get('/user/:login', async (req, res) => {
+    try {
+        const data = await getUser(req.params.login)
+        res.json(data.login)
+    } catch (err) {
+        res.status(err.response?.status || 500).json(err.response?.data || err.message)
+    }
 })
 
+app.get('/db-test', (req, res) => {
+    const rows = db.prepare('SELECT * FROM users').all()
+    res.json(rows)
+})
 
-//################################TEST#################################################
 app.get('/test', (req, res) => {
-	res.send('hello world')
-  })
+    res.send('hello world')
+})
 
-  app.listen(port, '0.0.0.0', () => console.log(`Server running on port ${port}`))
-//################################END OF TEST#################################################
-
-
+app.listen(port, '0.0.0.0', () => console.log(`Server running on port ${port}`))
